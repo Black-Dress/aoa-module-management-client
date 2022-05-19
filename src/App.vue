52e7d6ae561d0a66aa3952608a69065ca1e63672
black-dress <lcjemail@qq.com>
2022-05-19 17:24:23 +0800

    引入icon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <el-button type="primary" @click="connect">connect</el-button>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
 
