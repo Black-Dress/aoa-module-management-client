56902d0002c0293a64be22ec973eb7c6075bcd53
black-dress <lcjemail@qq.com>
2022-07-10 18:03:23 +0800

    初始化mqttURl

--- src/components/prism-editor.vue
+++ src/components/prism-editor.vue
@@ -1,5 +1,5 @@
 <template>
-    <prism-editor class="my-editor" :readonly="ariaReadonly" v-model="code" :highlight="highlighter" line-numbers>
+    <prism-editor class="my-editor" :readonly="ariaReadonly" v-model="getcode" :highlight="highlighter" line-numbers>
     </prism-editor>
 </template>
 <script>
@@ -11,38 +11,39 @@ import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-bash'
 import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
-import { mqttx } from '@/utils/mqttx';
+
 export default {
     components: {
         PrismEditor,
     },
-    props: ['ariaReadonly'],
+    props: ['ariaReadonly', 'initialCode'],
     data: () => ({
-        code: ""
     }),
     methods: {
         highlighter(code) {
             return highlight(code, languages.bash); // languages.<insert language> to return html with markup
         },
-        connect() {
-            mqttx.connect()
-        }
-
     },
+    computed: {
+        getcode() {
+            return this.initialCode
+        }
+    }
 }
 </script>
 <style>
 /* required class */
 .my-editor {
     /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
-    background: #2d2d2d;
-    color: #ccc;
+    background: #3c3e3e;
+    color: rgb(204, 195, 195);
 
     /* you must provide font-family font-size line-height. Example: */
     font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
     font-size: 14px;
     line-height: 1.5;
     padding: 5px;
+    border-radius: 2%;
 }
 
 /* optional class for removing the outline */
--- src/views/dash-board.vue
+++ src/views/dash-board.vue
@@ -1,17 +1,41 @@
 <template>
     <div>
-        <p1 :aria-readonly="false"></p1>
+        <div>
+            <el-row style="margin-bottom: 10px;">
+                <el-col :span="4">
+                    MQTT服务器
+                </el-col>
+            </el-row>
+            <el-row :gutter="20">
+                <el-col :span="20">
+                    <div>
+                        <p1 :aria-readonly="false" :initial-code="mqttUrl" />
+                    </div>
+                </el-col>
+                <el-col :span="4">
+                    <el-button id="start" type="primary" @click="connect()">连接</el-button>
+                </el-col>
+            </el-row>
+        </div>
         <el-divider></el-divider>
-        <p2 :aria-readonly="true"></p2>
+        <p2 :aria-readonly="true" />
     </div>
 </template>
 <script>
-import p1 from "../components/prism-editor.vue";
+import p1 from "../components/prism-editor.vue"
 import p2 from "../components/prism-editor.vue"
-
+// import { mqttx } from '@/utils/mqttx';
 export default {
     components: {
         p1, p2
+    },
+    data: () => ({
+        mqttUrl: "aaa",
+    }),
+    methods: {
+        connect() {
+            console.log("connect")
+        }
     }
 }
 </script>
