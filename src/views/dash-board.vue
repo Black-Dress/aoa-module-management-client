0bb571424a2a4006a1135150f3242278f1720f3c
black-dress <lcjemail@qq.com>
2022-07-11 23:00:31 +0800

    todo

--- src/views/dash-board.vue
+++ src/views/dash-board.vue
@@ -1,88 +1,61 @@
 <template>
-  <div>
     <div>
-      <el-row :gutter="20" style="margin-bottom: 10px">
-        <el-col :span="4">
-          <span style="float: left">MQTT服务器</span>
-        </el-col>
-      </el-row>
-      <el-row :gutter="10">
-        <el-col :span="4">
-          <el-select v-model="curUrl" placeholder="URL">
-            <el-option
-              v-for="item in mqttUrls"
-              :key="item.name"
-              :label="item.name"
-              :value="item.value"
-            />
-          </el-select>
-        </el-col>
-        <el-col :span="15">
-          <el-input
-            v-model="curUrl"
-            :placeholder="curUrl"
-            aria-readonly="true"
-          />
-        </el-col>
-        <el-col :span="2">
-          <el-button @click="addUrlDialogVisible = true">
-            <el-icon>
-              <plus></plus>
-            </el-icon>
-          </el-button>
-          <el-dialog
-            v-model="addUrlDialogVisible"
-            title="添加新链接"
-            width="300px"
-          >
+        <div>
+            <el-row :gutter="20" style="margin-bottom: 10px">
+                <el-col :span="4">
+                    <span style="float: left">MQTT服务器</span>
+                </el-col>
+            </el-row>
+            <el-row :gutter="10">
+                <el-col :span="4">
+                    <el-select v-model="curUrl" placeholder="URL">
+                        <el-option v-for="item in mqttUrls" :key="item.name" :label="item.name" :value="item.value" />
+                    </el-select>
+                </el-col>
+                <el-col :span="15">
+                    <el-input v-model="curUrl" :placeholder="curUrl" aria-readonly="true" />
+                </el-col>
+                <el-col :span="2">
+                    <el-button @click="addUrlDialogVisible = true">
+                        <el-icon>
+                            <plus></plus>
+                        </el-icon>
+                    </el-button>
+                    <el-dialog v-model="addUrlDialogVisible" title="添加新链接" width="300px">
+                        <el-row>
+                            <el-col>
+                                <el-form :v-model="newUrl">
+                                    <el-form-item label="名称" label-width="20%">
+                                        <el-input v-model="newUrl.name"></el-input>
+                                    </el-form-item>
+                                    <el-form-item label="地址" label-width="20%">
+                                        <el-input v-model="newUrl.value"></el-input>
+                                    </el-form-item>
+                                </el-form>
+                            </el-col>
+                        </el-row>
+                        <el-row :gutter="3">
+                            <el-col :span="12">
+                                <el-button @click="dialogCancel">取消</el-button>
+                            </el-col>
+                            <el-col :span="12">
+                                <el-button type="primary" @click="dialogConfirm">确认</el-button>
+                            </el-col>
+                        </el-row>
+                    </el-dialog>
+                </el-col>
+                <el-col :span="3">
+                    <el-button id="start" type="primary" @click="connect(curUrl)" style="width: 90px">连接</el-button>
+                </el-col>
+            </el-row>
+        </div>
+        <el-divider></el-divider>
+        <div>
             <el-row>
-              <el-col>
-                <el-form :v-model="newUrl">
-                  <el-form-item label="名称" label-width="20%">
-                    <el-input v-model="newUrl.name"></el-input>
-                  </el-form-item>
-                  <el-form-item label="地址" label-width="20%">
-                    <el-input v-model="newUrl.value"></el-input>
-                  </el-form-item>
-                </el-form>
-              </el-col>
+                <prism-editor class="my-editor" :readonly="true" v-model="code" :highlight="highlighter" line-numbers />
             </el-row>
-            <el-row :gutter="3">
-              <el-col :span="12">
-                <el-button @click="dialogCancel">取消</el-button>
-              </el-col>
-              <el-col :span="12">
-                <el-button type="primary" @click="dialogConfirm"
-                  >确认</el-button
-                >
-              </el-col>
-            </el-row>
-          </el-dialog>
-        </el-col>
-        <el-col :span="3">
-          <el-button
-            id="start"
-            type="primary"
-            @click="connect(curUrl)"
-            style="width: 90px"
-            >连接</el-button
-          >
-        </el-col>
-      </el-row>
+        </div>
     </div>
-    <el-divider></el-divider>
-    <div>
-      <el-row>
-        <prism-editor
-          class="my-editor"
-          :readonly="true"
-          v-model="code"
-          :highlight="highlighter"
-          line-numbers
-        />
-      </el-row>
-    </div>
-  </div>
 </template>
 <script>
 import { PrismEditor } from "vue-prism-editor";
@@ -97,90 +70,90 @@ import * as mqtt from "mqtt";
 const ipcRenderer = window.require("electron").ipcRenderer;
 
 export default {
-  components: {
-    PrismEditor,
-  },
-  created: function () {
-    this.init();
-  },
-  mounted: function () {},
-  data: function () {
-    return {
-      mqttUrls: [],
-      curUrl: "",
-      code: "",
-      addUrlDialogVisible: false,
-      newUrl: {
-        name: "",
-        value: "",
-      },
-    };
-  },
-  methods: {
-    connect(url) {
-      if (url == undefined || url == "") {
-        ElMessage({
-          message: "请选择mqtt服务器地址。",
-          type: "warning",
-        });
-        return;
-      }
-      // FIXME 为什么不能够使用mqtt协议
-      mqtt
-        .connect("mqtt://broker.emqx.io:1883/mqtt", {
-          // host: "broker.emqx.io",
-          // port: 1883,
-          protocol: "mqtt",
-          reconnectPeriod: 0,
-          clientId: "emqx_test",
-          username: "emqx_test",
-          password: "emqx_test",
-        })
-        .on("connect", () => {
-          console.log("success");
-        });
+    components: {
+        PrismEditor,
     },
-    highlighter(code) {
-      return highlight(code, languages.plaintext, "bash");
+    created: function () {
+        this.init();
     },
-    init() {
-      // 读取配置文件
-      ipcRenderer.on("mqtt", (event, data) => {
-        this.mqttUrls = data;
-      });
-      ipcRenderer.send("read", "mqtt");
+    mounted: function () { },
+    data: function () {
+        return {
+            mqttUrls: [],
+            curUrl: "",
+            code: "",
+            addUrlDialogVisible: false,
+            newUrl: {
+                name: "",
+                value: "",
+            },
+        };
     },
-    dialogCancel() {
-      this.newUrl = {};
-      this.addUrlDialogVisible = false;
+    methods: {
+        connect(url) {
+            if (url == undefined || url == "") {
+                ElMessage({
+                    message: "请选择mqtt服务器地址。",
+                    type: "warning",
+                });
+                return;
+            }
+            // TODO 为什么不能够使用mqtt协议
+            mqtt
+                .connect("mqtt://broker.emqx.io:1883/mqtt", {
+                    // host: "broker.emqx.io",
+                    // port: 1883,
+                    protocol: "mqtt",
+                    reconnectPeriod: 0,
+                    clientId: "emqx_test",
+                    username: "emqx_test",
+                    password: "emqx_test",
+                })
+                .on("connect", () => {
+                    console.log("success");
+                });
+        },
+        highlighter(code) {
+            return highlight(code, languages.plaintext, "bash");
+        },
+        init() {
+            // 读取配置文件
+            ipcRenderer.on("mqtt", (event, data) => {
+                this.mqttUrls = data;
+            });
+            ipcRenderer.send("read", "mqtt");
+        },
+        dialogCancel() {
+            this.newUrl = {};
+            this.addUrlDialogVisible = false;
+        },
+        dialogConfirm() {
+            this.mqttUrls.push(this.newUrl);
+            this.newUrl = {};
+            this.addUrlDialogVisible = false;
+            ipcRenderer.send("writeMqtt", JSON.stringify(this.mqttUrls));
+        },
     },
-    dialogConfirm() {
-      this.mqttUrls.push(this.newUrl);
-      this.newUrl = {};
-      this.addUrlDialogVisible = false;
-      ipcRenderer.send("writeMqtt", JSON.stringify(this.mqttUrls));
-    },
-  },
 };
 </script>
 <style>
 /* required class */
 .my-editor {
-  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
-  background: #2d2d2d;
-  color: #ccc;
+    /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+    background: #2d2d2d;
+    color: #ccc;
 
-  /* you must provide font-family font-size line-height. Example: */
-  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
-  font-size: 14px;
-  line-height: 1.5;
-  padding: 5px;
-  border-radius: 5px;
-  max-height: 500px;
+    /* you must provide font-family font-size line-height. Example: */
+    font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+    font-size: 14px;
+    line-height: 1.5;
+    padding: 5px;
+    border-radius: 5px;
+    max-height: 500px;
 }
 
 /* optional class for removing the outline */
 .prism-editor__textarea:focus {
-  outline: none;
+    outline: none;
 }
 </style>
