71a9f521bd402e7bee18353825990042754edefa
black-dress <lcjemail@qq.com>
2022-07-31 16:07:58 +0800

    station 添加状态显示

--- src/views/station-view.vue
+++ src/views/station-view.vue
@@ -41,6 +41,9 @@
                 <el-row>
                   <code>station id: {{ col.id }}</code>
                 </el-row>
+                <el-row>
+                  <code>status: {{ col.status ? "online" : "offline" }}</code>
+                </el-row>
               </div>
             </el-card>
           </el-col>
@@ -64,6 +67,12 @@
             </el-form-item>
             <el-form-item label="net" label-width="20%">
               <el-input v-model="newStation.net"></el-input>
+            </el-form-item>
+            <el-form-item label="status" label-width="20%">
+              <el-radio-group v-model="newStation.status">
+                <el-radio :label="true">online</el-radio>
+                <el-radio :label="false">offline</el-radio>
+              </el-radio-group>
             </el-form-item>
           </el-col>
         </el-row>
@@ -113,6 +122,7 @@ export default {
                 y: 1,
                 z: 1,
               },
+              status: true,
               net: "192.168.1.101",
             },
             {},
@@ -125,23 +135,31 @@ export default {
         id: "",
         name: "",
         net: "",
+        status: true,
         position: { x: 0, y: 0, z: 0 },
       },
       addStationDialogVisible: false,
+      row_size: 3,
+      col_size: 4,
     };
   },
   created: function () {
     this.stations = this.toStations(this.$mqttx.station_list);
   },
+  computed: {
+    page_size() {
+      return this.row_size * this.col_size;
+    },
+  },
   methods: {
     toStations(data) {
       let res = {};
-      for (let i = 0; i * 12 < data.length; i += 1) {
+      for (let i = 0; i * this.page_size < data.length; i += 1) {
         res[i + 1] = [[], [], []];
         let index = 0;
-        for (let j = 0; j < 12 && j + i * 12 < data.length; j += 1) {
+        for (let j = 0; j < this.page_size && j + i * this.page_size < data.length; j += 1) {
           if (j % 4 == 0 && j != 0) index += 1;
-          res[i + 1][index].push(data[j + i * 12]);
+          res[i + 1][index].push(data[j + i * this.page_size]);
         }
       }
       return res;
@@ -151,24 +169,25 @@ export default {
     },
     remove(i, j) {
       this.stations[this.current_page][i].splice(j, 1);
-      this.$mqttx.station_list.splice((this.current_page - 1) * 12 + i * 4 + j, 1);
+      this.$mqttx.station_list.splice((this.current_page - 1) * this.page_size + i * this.col_size + j, 1);
       ipcRenderer.send("write", ["station", JSON.stringify(this.$mqttx.station_list)]);
       this.total -= 1;
     },
     confirm() {
       let l = this.$mqttx.station_list.length;
       if (l % 12 == 0) {
-        this.stations[Number.parseInt(l / 12) + 1] = [[this.newStation], [], []];
+        this.stations[Number.parseInt(l / this.page_size) + 1] = [[this.newStation], [], []];
         this.current_page += 1;
       } else {
-        this.stations[Number.parseInt(l / 12) + 1][(l % 12) % 3].push(this.newStation);
+        this.stations[Number.parseInt(l / this.page_size) + 1][(l % this.page_size) % this.row_size].push(this.newStation);
       }
       this.$mqttx.station_list.push(this.newStation);
-      this.total = this.$mqttx.station_list.length;
+      this.total += 1;
       this.newStation = {
         id: "",
         name: "",
         net: "",
+        status: true,
         position: { x: 0, y: 0, z: 0 },
       };
       this.addStationDialogVisible = false;
@@ -179,6 +198,7 @@ export default {
         id: "",
         name: "",
         net: "",
+        status: true,
         position: { x: 0, y: 0, z: 0 },
       };
       this.addStationDialogVisible = false;
