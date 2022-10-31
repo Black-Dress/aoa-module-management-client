cb2a2cdd72071310a21caad7d56995b8a61a73ff
black-dress <lcjemail@qq.com>
2022-10-31 11:17:13 +0800

    fix dashboard

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import router from "./router";
 import "./assert/css/index.css";
-import { mqttx } from "./utils/mqttx";
+import {mqttx} from "./utils/mqttx";
+
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
+    app.component(key, component);
 }
 app.config.globalProperties.$mqttx = mqttx;
 // 使用router
--- src/views/dash-board.vue
+++ src/views/dash-board.vue
@@ -13,18 +13,20 @@
       </el-col>
       <el-col :span="2" style="text-align: left">
         <el-button @click="editIdDialogVisible = true" text>
-          <el-icon> <Edit /> </el-icon>
+          <el-icon>
+            <Edit/>
+          </el-icon>
         </el-button>
       </el-col>
     </el-row>
     <el-row :gutter="5">
       <el-col :span="3">
         <el-select v-model="cur_url" placeholder="URL">
-          <el-option v-for="item in this.client.urls" :key="item.name" :label="item.name" :value="item" />
+          <el-option v-for="item in this.client.urls" :key="item.name" :label="item.name" :value="item"/>
         </el-select>
       </el-col>
       <el-col :span="14">
-        <el-input v-model="cur_url.value" disabled />
+        <el-input v-model="cur_url.value" disabled/>
       </el-col>
       <el-col :span="2">
         <el-button @click="addUrlDialogVisible = true">
@@ -36,18 +38,21 @@
       <el-col :span="2">
         <el-button @click="rmUrls">
           <el-icon>
-            <Minus />
+            <Minus/>
           </el-icon>
         </el-button>
       </el-col>
       <el-col :span="2" style="max-width: 32px">
-        <el-button v-if="!this.status" id="start" type="primary" @click="connect(cur_url.value)"> connect </el-button>
+        <el-button v-if="!this.status" id="start" type="primary" @click="connect(cur_url.value)"> connect</el-button>
         <el-button v-if="this.status" id="end" type="danger" @click="disconnect()">disconnect</el-button>
       </el-col>
     </el-row>
     <el-divider></el-divider>
     <el-row class="main_row">
-      <prism-editor :key="code" class="my-editor" :readonly="true" :model-value="code" :highlight="highlighter" line-numbers />
+      <!--suppress JSValidateTypes -->
+      <prism-editor :key="code" class="my-editor" :readonly="true" :model-value="code"
+                    :highlight="highlighter"
+                    line-numbers/>
     </el-row>
   </el-main>
 
@@ -75,16 +80,20 @@
   </el-dialog>
   <el-dialog v-model="editIdDialogVisible" title="UPDATE CLIENT" width="450px" @close="dialogCancel">
     <el-row>
-      <el-col class="dialog-col" :span="4"> <p>clientId</p> </el-col>
+      <el-col class="dialog-col" :span="4"><p>clientId</p></el-col>
       <el-col :span="17">
         <el-input v-model="this.client.clientId"></el-input>
       </el-col>
       <el-col :span="3">
         <el-button v-if="!name_check_status" @click="check_client_id(this.client.clientId)">
-          <el-icon><RefreshLeft /></el-icon>
+          <el-icon>
+            <RefreshLeft/>
+          </el-icon>
         </el-button>
         <el-button v-if="name_check_status" type="primary">
-          <el-icon><Check /></el-icon>
+          <el-icon>
+            <Check/>
+          </el-icon>
         </el-button>
       </el-col>
     </el-row>
@@ -129,17 +138,18 @@
   </el-dialog>
 </template>
 <script>
-import { PrismEditor } from "vue-prism-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
-import { ElMessage } from "element-plus";
-import { mqttx } from "@/utils/mqttx";
-import { store } from "@/utils/store";
-const ipcRenderer = window.require("electron").ipcRenderer;
+import {PrismEditor} from "vue-prism-editor";
+import {highlight, languages} from "prismjs/components/prism-core";
+import {ElMessage} from "element-plus";
+import {mqttx} from "@/utils/mqttx";
+import {store} from "@/utils/store";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-bash";
 import "prismjs/themes/prism-tomorrow.css";
 import "vue-prism-editor/dist/prismeditor.min.css";
-import { validateId, upload_client } from "@/api/client";
+import {upload_client, validateId} from "@/api/client";
+
+const ipcRenderer = window.require("electron").ipcRenderer;
 
 export default {
   components: {
@@ -153,18 +163,17 @@ export default {
     ipcRenderer.send("read", ["mqtt"]);
     if (this.status) this.connect(this.cur_url.value);
   },
-  mounted: function () {},
   data: function () {
     return {
       addUrlDialogVisible: false,
       editIdDialogVisible: false,
-      new_url: { name: "", value: "" },
+      new_url: {name: "", value: ""},
       code: "",
       client: {
         clientId: "id",
         name: "",
         project: "",
-        urls: [{ name: "", value: "" }],
+        urls: [{name: "", value: ""}],
         position: "",
         contact: "",
         remark: "",
@@ -195,22 +204,22 @@ export default {
     // TODO 统一mqtt代码
     upload_client() {
       upload_client(
-        this.client,
-        () => {
-          ElMessage({ type: "success", message: "upload success!" });
-          this.editIdDialogVisible = false;
-          ipcRenderer.send("write", ["mqtt", JSON.stringify(this.client)]);
-          mqttx.setId(this.client.clientId);
-        },
-        (e) => {
-          console.error(e);
-        }
+          this.client,
+          () => {
+            ElMessage({type: "success", message: "upload success!"});
+            this.editIdDialogVisible = false;
+            ipcRenderer.send("write", ["mqtt", JSON.stringify(this.client)]);
+            mqttx.setId(this.client.clientId);
+          },
+          (e) => {
+            console.error(e);
+          }
       );
     },
     check_client_id(name) {
       validateId(name, (res) => {
-        if (res.code != 200) {
-          ElMessage({ type: "error", message: res.msg });
+        if (res.code !== 200) {
+          ElMessage({type: "error", message: res.msg});
           return;
         }
         this.clientId = res.data;
@@ -221,30 +230,31 @@ export default {
       this.$mqttx.disconnect(this.df);
     },
     connect(url) {
-      if (url == undefined || url == "") {
-        ElMessage({ message: "choose a mqtt server adderss", type: "warning" });
+      if (url === undefined || url === "") {
+        ElMessage({message: "choose a mqtt server address", type: "warning"});
         return;
       }
       // 连接
       this.$mqttx.connect(url, this.s, this.f);
     },
     df() {
-      ElMessage({ type: "success", message: "disconnect success" });
+      ElMessage({type: "success", message: "disconnect success"});
       this.status = false;
       this.code = "";
     },
     s(msg) {
-      ElMessage({ type: "success", message: "connect success" });
+      ElMessage({type: "success", message: "connect success"});
       this.$mqttx.set_message_callback((topic, ms) => {
         this.code += ms.toString() + "\n";
       });
       this.code += msg + "\n";
       // 自动订阅主题
-      this.$mqttx.defaultSubscribe(() => {});
+      this.$mqttx.defaultSubscribe(() => {
+      });
       store.set_main_connect_status(true);
     },
     f(msg) {
-      ElMessage({ type: "error", message: "connect failed" });
+      ElMessage({type: "error", message: "connect failed"});
       this.code += msg + "\n";
       store.set_main_connect_status(false);
     },
@@ -258,22 +268,22 @@ export default {
       this.name_check_status = false;
     },
     dialogConfirm() {
-      if (this.new_url.name != "" && this.new_url.value != "") {
-        if (this.client.urls.findIndex((item) => item.name == this.new_url.name) != -1) {
-          ElMessage({ type: "warning", message: "there is already a same name" });
+      if (this.new_url.name !== "" && this.new_url.value !== "") {
+        if (this.client.urls.findIndex((item) => item.name === this.new_url.name) !== -1) {
+          ElMessage({type: "warning", message: "there is already a same name"});
           return;
         }
         this.client.urls.push(this.new_url);
         ipcRenderer.send("write", ["mqtt", JSON.stringify(this.client)]);
-        ElMessage({ type: "success", message: "add url success" });
-        this.new_url = { name: "", value: "" };
+        ElMessage({type: "success", message: "add url success"});
+        this.new_url = {name: "", value: ""};
       }
       this.addUrlDialogVisible = false;
     },
     rmUrls() {
-      var index = this.client.urls.findIndex((item) => item.name == this.cur_url.name);
+      const index = this.client.urls.findIndex((item) => item.name === this.cur_url.name);
       this.client.urls.splice(index, 1);
-      this.cur_url = this.client.urls.length > 0 ? this.client.urls[0] : { name: "", value: "" };
+      this.cur_url = this.client.urls.length > 0 ? this.client.urls[0] : {name: "", value: ""};
       this.dialogConfirm();
     },
   },
@@ -282,7 +292,7 @@ export default {
 <style>
 /* required class */
 .my-editor {
-  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+  /* we dont use `language-` classes anymore so that's why we need to add background and text color manually */
   background: #2d2d2d;
   color: #ccc;
 
@@ -295,35 +305,37 @@ export default {
   max-height: 450px;
 }
 
-/* optional class for removing the outline */
-.prism-editor__textarea:focus {
-  outline: none;
-}
-.el-col {
+
+el-col {
   display: block;
   margin: 0 0 0 0;
   padding: 0 0 0 0;
   float: left;
   height: 36px;
 }
+
 p {
   height: 18px;
   padding: 0 0 0 0;
 }
-.el-col p {
+
+el-col p {
   margin-top: 9px;
   text-align: left;
   font-weight: 600;
 }
-.el-col .el-button {
+
+el-col el-button {
   height: 32px;
   margin-top: 3px;
 }
-.el-col .el-input {
+
+el-col el-input {
   height: 32px;
   margin-top: 3px;
 }
-.el-row {
+
+el-row {
   margin-bottom: 10px;
 }
 </style>
