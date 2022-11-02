38e25d56e23b0f72514cebb7a5d6d312a7fd447d
black-dress <lcjemail@qq.com>
2022-11-02 10:46:35 +0800

    fix station-view.vue 保存stations

--- src/views/station-view.vue
+++ src/views/station-view.vue
@@ -15,10 +15,10 @@
       </el-row>
       <el-divider></el-divider>
     </el-header>
-    <el-main style="margin-top: 10px">
-      <el-row class="row" :gutter="10" v-for="(a,i) in this.row_size" :key="i">
-        <el-col class="col" v-for="(b,j) in this.col_size" :key="j" :span="24/this.col_size">
-          <el-card v-if="index(i,j) < this.total"
+    <el-main :key="stations.length">
+      <el-row class="row" :gutter="10" v-for="i in this.row_size" :key="i">
+        <el-col class="col" v-for="j in this.col_size" :key="j" :span="24/this.col_size">
+          <el-card v-if="index(i,j) < stations.length"
                    :body-style="{ padding: '0px' }"
                    class="card" shadow="hover"
                    :class="this.stations[this.index(i,j)].status?'active':'inactive'"
@@ -54,50 +54,49 @@
     </el-main>
     <el-footer>
       <el-row justify="center" style="margin-top: 10px">
-        <el-pagination layout="prev, pager, next" background :total="total" :page-size="page_size" :pager-count="7"
+        <el-pagination layout="prev, pager, next" background :total="stations.length" :page-size="page_size"
+                       :pager-count="7"
                        v-model:current-page="current_page"/>
       </el-row>
     </el-footer>
   </el-container>
   <el-dialog v-model="addStationDialogVisible" title="add new station" width="400px">
-    <el-form :model="new_station">
-      <el-row>
-        <el-col>
-          <el-form-item label="id" label-width="20%">
-            <el-input v-model="new_station.id"></el-input>
-          </el-form-item>
-          <el-form-item label="name" label-width="20%">
-            <el-input v-model="new_station.name"></el-input>
-          </el-form-item>
-          <el-form-item label="net" label-width="20%">
-            <el-input v-model="new_station.net"></el-input>
-          </el-form-item>
-          <el-form-item label="status" label-width="20%">
-            <el-radio-group v-model="new_station.status">
-              <el-radio :label="true">online</el-radio>
-              <el-radio :label="false">offline</el-radio>
-            </el-radio-group>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row justify="center" :gutter="10">
-        <el-col :span="6">
-          <el-form-item label="x:" label-width="20%">
-            <el-input v-model="new_station.position.x"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="6">
-          <el-form-item label="y:" label-width="20%">
-            <el-input v-model="new_station.position.y"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="6">
-          <el-form-item label="z:" label-width="20%">
-            <el-input v-model="new_station.position.z"></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-    </el-form>
+    <el-row>
+      <el-col :span="4"><p>id</p></el-col>
+      <el-col :span="20">
+        <el-input v-model="new_station.id"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4"><p>name</p></el-col>
+      <el-col :span="20">
+        <el-input v-model="new_station.name"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4"><p>net</p></el-col>
+      <el-col :span="20">
+        <el-input v-model="new_station.net"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6"><p>station_x</p></el-col>
+      <el-col :span="18">
+        <el-input v-model="new_station.position.x"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6"><p>station_y</p></el-col>
+      <el-col :span="18">
+        <el-input v-model="new_station.position.y"></el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6"><p>station_z</p></el-col>
+      <el-col :span="18">
+        <el-input v-model="new_station.position.z"></el-input>
+      </el-col>
+    </el-row>
     <el-row :gutter="3">
       <el-col :span="12">
         <el-button @click="cancel">cancel</el-button>
@@ -109,35 +108,19 @@
   </el-dialog>
 </template>
 <script>
+import {ElMessage} from "element-plus";
+
 const ipcRenderer = window.require("electron").ipcRenderer;
 export default {
   data: function () {
     return {
       current_page: 1,
-      stations: {
-        1: [
-          [
-            {
-              id: "a",
-              position: {
-                x: 1,
-                y: 1,
-                z: 1,
-              },
-              status: true,
-              net: "192.168.1.101",
-            },
-            {},
-          ],
-          [],
-          [],
-        ],
-      },
+      stations: [],
       new_station: {
         id: "",
         name: "",
         net: "",
-        status: true,
+        status: false,
         position: {x: 0, y: 0, z: 0},
       },
       addStationDialogVisible: false,
@@ -146,6 +129,7 @@ export default {
     };
   },
   created: function () {
+    // 将对象拷贝过来了，对stations的修改会反馈到station_list上
     this.stations = this.$mqttx.station_list
   },
   computed: {
@@ -182,31 +166,37 @@ export default {
      */
     remove(i, j) {
       this.stations.splice(this.index(i, j), 1);
-      this.$mqttx.station_list.splice(this.index(i, j), 1);
-      ipcRenderer.send("write", ["station", JSON.stringify(this.$mqttx.station_list)]);
+      ipcRenderer.send("write", ["stations", JSON.stringify(this.$mqttx.station_list)]);
     },
     /**
      * 确认添加基站
      */
     confirm() {
+      if (this.new_station.id === "" || this.new_station.net === "") {
+        ElMessage({type: "error", message: "id or net could not be empty!"})
+        return
+      }
+      if (this.stations.findIndex(s => s.id === this.new_station.id || s.net === this.new_station.net) !== -1) {
+        ElMessage({type: "error", message: "id or net could not be repeated"})
+        return;
+      }
       this.stations.push(this.new_station)
-      this.$mqttx.station_list.push(this.new_station);
       this.new_station = {
         id: "",
         name: "",
         net: "",
-        status: true,
+        status: false,
         position: {x: 0, y: 0, z: 0},
       };
       this.addStationDialogVisible = false;
-      ipcRenderer.send("write", ["stations", JSON.stringify(this.$mqttx.station_list)]);
+      ipcRenderer.send("write", ["stations", JSON.stringify(this.stations)]);
     },
     cancel() {
       this.new_station = {
         id: "",
         name: "",
         net: "",
-        status: true,
+        status: false,
         position: {x: 0, y: 0, z: 0},
       };
       this.addStationDialogVisible = false;
--- src/views/tag-view.vue
+++ src/views/tag-view.vue
@@ -1,58 +1,58 @@
 <template>
-  <div>
-    <el-container>
-      <el-header>
-        <el-row justify="space-between">
-          <el-col :span="6" style="text-align: left">
-            <h1>AoA tags</h1>
-          </el-col>
-          <el-col :span="4">
-            <el-button type="primary" @click="addStationDialogVisible = true">
-              <el-icon>
-                <plus />
-              </el-icon>
-            </el-button>
-          </el-col>
-        </el-row>
-        <el-divider></el-divider>
-      </el-header>
-      <el-main style="margin-top: 10px">
-        <el-row :gutter="10" v-for="(row, i) in this.tags[this.current_page]" :key="i">
-          <el-col v-for="(col, j) in row" :key="j" :span="6">
-            <el-card :body-style="{ padding: '0px' }" class="card" shadow="hover">
-              <div style="padding: 10px">
-                <el-row justify="space-between" class="row">
-                  <el-col :span="10" style="text-align: left">
-                    <el-button text @click="detail(col.id)" style="padding: 0 0 0 0">
-                      <h1>{{ col.name }}</h1>
-                    </el-button>
-                  </el-col>
-                  <el-col :span="3">
-                    <el-button text @click="remove(i, j)">
-                      <el-icon>
-                        <Close />
-                      </el-icon>
-                    </el-button>
-                  </el-col>
-                </el-row>
-                <el-row class="row">
-                  <code> Tag Status: {{ col.status ? "online" : "offline" }} </code>
-                </el-row>
-                <el-row class="row">
-                  <code>Tag id: {{ col.id }}</code>
-                </el-row>
-              </div>
-            </el-card>
-          </el-col>
-        </el-row>
-      </el-main>
-      <el-footer>
-        <el-row justify="center" style="margin-top: 10px">
-          <el-pagination layout="prev, pager, next" background :total="total" :page-size="page_size" :pager-count="7" v-model:current-page="current_page" />
-        </el-row>
-      </el-footer>
-    </el-container>
-  </div>
+
+  <el-container>
+    <el-header>
+      <el-row justify="space-between">
+        <el-col :span="6" style="text-align: left">
+          <h1>AoA tags</h1>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="addStationDialogVisible = true">
+            <el-icon>
+              <plus/>
+            </el-icon>
+          </el-button>
+        </el-col>
+      </el-row>
+      <el-divider></el-divider>
+    </el-header>
+    <el-main style="margin-top: 10px">
+      <el-row :gutter="10" v-for="(row, i) in this.tags[this.current_page]" :key="i">
+        <el-col v-for="(col, j) in row" :key="j" :span="6">
+          <el-card :body-style="{ padding: '0px' }" class="card" shadow="hover">
+            <div style="padding: 10px">
+              <el-row justify="space-between" class="row">
+                <el-col :span="10" style="text-align: left">
+                  <el-button text @click="detail(col.id)" style="padding: 0 0 0 0">
+                    <h1>{{ col.name }}</h1>
+                  </el-button>
+                </el-col>
+                <el-col :span="3">
+                  <el-button text @click="remove(i, j)">
+                    <el-icon>
+                      <Close/>
+                    </el-icon>
+                  </el-button>
+                </el-col>
+              </el-row>
+              <el-row class="row">
+                <code> Tag Status: {{ col.status ? "online" : "offline" }} </code>
+              </el-row>
+              <el-row class="row">
+                <code>Tag id: {{ col.id }}</code>
+              </el-row>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer>
+      <el-row justify="center" style="margin-top: 10px">
+        <el-pagination layout="prev, pager, next" background :total="total" :page-size="page_size" :pager-count="7"
+                       v-model:current-page="current_page"/>
+      </el-row>
+    </el-footer>
+  </el-container>
 </template>
 <script>
 const ipcRenderer = window.require("electron").ipcRenderer;
@@ -61,7 +61,7 @@ export default {
   data: function () {
     return {
       tags: {
-        1: [[{ name: "aa", id: "id-aa", status: false }, {}], [], []],
+        1: [[{name: "aa", id: "id-aa", status: false}, {}], [], []],
       },
       tag_list: [],
       total: this.$mqttx.tag_list.length,
@@ -86,7 +86,7 @@ export default {
       this.total -= 1;
     },
     detail(id) {
-      this.$router.push({ name: "tag-details", query: { id: id } });
+      this.$router.push({name: "tag-details", query: {id: id}});
     },
     toTags(args) {
       let res = {};
@@ -94,7 +94,7 @@ export default {
         res[i + 1] = [[], [], []];
         let index = 0;
         for (let j = 0; j < this.page_size && j + i * this.page_size < args.length; j += 1) {
-          if (j % 4 == 0 && j != 0) index += 1;
+          if (j % 4 === 0 && j !== 0) index += 1;
           res[i + 1][index].push(args[j + i * this.page_size]);
         }
       }
