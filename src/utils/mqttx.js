43dd0a62f73ad46b8a115a42663bec7bc37bf00d
black-dress <lcjemail@qq.com>
2023-01-24 22:01:44 +0800

    fix 读取文件目录的递归函数

--- src/utils/mqttx.js
+++ src/utils/mqttx.js
@@ -1,214 +1,226 @@
 import * as mqtt from "mqtt";
-import { upload_aoa_raw_data } from "@/api/client";
+import {upload_aoa_raw_data} from "@/api/client";
 
 const ipcRenderer = window.require("electron").ipcRenderer;
 const MAX_LEN = 100000;
 
 export class mqttx {
-  static options = {
-    clientId: "aoa_client",
-    clean: true,
-    reconnectPeriod: 0,
-    connectTimeout: 1000,
-  };
-  static url = "ws://localhost:9001";
-  static client = null;
-  // 存储所有的tag，station 对象
-  static tag_list = [
-    {
-      name: "aa",
-      id: "aa-id",
-      status: false,
-    },
-    {
-      name: "aa",
-      id: "aa-id",
-      status: false,
-    },
-  ];
-  static station_list = [
-    {
-      id: "a",
-      name: "别称",
-      position: {
-        x: 1,
-        y: 1,
-        z: 1,
-      },
-      status: false,
-      net: "192.168.1.101",
-    },
-  ];
-  static station_map = new Map();
-  /**
-   * 启动的基站数量
-   * @type {number}
-   */
-  static active_station_num = 2;
-  /**
-   * 每个基站应该能够存储的数据数量，达到这个数据量之后上传数据
-   * @type {number}
-   */
-  static msg_station_size = 3;
-  /**
-   * 存储采集的数据，上传至服务器
-   * @type {[]}
-   */
-  static msgs = [];
-  /**
-   * 按照基站id进行存储
-   * @type {*}
-   */
-  static stations = new Map();
-  /**
-   * 按照tag id 进行存储
-   * @type {*}
-   */
-  static tags = new Map();
-  /**
-   * 激活的 tag id,能够发送数据的 tag id
-   * @type {*}
-   */
-  static active_tag = mqttx.tag_list[0].id;
-  // 默认回调函数
-  static messages = function (topic, message) {
-    console.log(topic, message);
-  };
+    static options = {
+        clientId: "aoa_client",
+        clean: true,
+        reconnectPeriod: 0,
+        connectTimeout: 1000,
+    };
+    static url = "ws://localhost:9001";
+    static client = null;
+    // 存储所有的tag，station 对象
+    static tag_list = [
+        {
+            name: "aa",
+            id: "aa-id",
+            status: false,
+        },
+        {
+            name: "aa",
+            id: "aa-id",
+            status: false,
+        },
+    ];
+    static station_list = [
+        {
+            id: "a",
+            name: "别称",
+            position: {
+                x: 1,
+                y: 1,
+                z: 1,
+            },
+            status: false,
+            net: "192.168.1.101",
+        },
+    ];
+    static station_map = new Map();
+    /**
+     * 启动的基站数量
+     * @type {number}
+     */
+    static active_station_num = 2;
+    /**
+     * 每个基站应该能够存储的数据数量，达到这个数据量之后上传数据
+     * @type {number}
+     */
+    static msg_station_size = 3;
+    /**
+     * 存储采集的数据，上传至服务器
+     * @type {[]}
+     */
+    static msgs = [];
+    /**
+     * 按照基站id进行存储
+     * @type {*}
+     */
+    static stations = new Map();
+    /**
+     * 按照tag id 进行存储
+     * @type {*}
+     */
+    static tags = new Map();
+    /**
+     * 激活的 tag id,能够发送数据的 tag id
+     * @type {*}
+     */
+    static active_tag = mqttx.tag_list[0].id;
+    // 默认回调函数
+    static messages = function (topic, message) {
+        console.log(topic, message);
+    };
 
-  /**
-   * mqtt 连接服务器函数
-   * @param url{string} 连接地质
-   * @param s{function} 连接成功回调
-   * @param f{function} 连接失败回调
-   */
-  static connect(url = "ws://localhost:9001", s = function () {}, f = function () {}) {
-    mqttx.url = url;
-    mqttx.client = mqtt.connect(mqttx.url, mqttx.options);
-    mqttx.client.on("connect", () => {
-      s();
-      this.client.on("message", mqttx.message);
-    });
-    mqttx.client.on("error", (err) => {
-      console.log("client error", err);
-    });
-    mqttx.sleep(300).then(() => {
-      if (mqttx.client.connected === false) {
-        f();
-        // 重新启动 mosquitto
-        ipcRenderer.send("mosquitto_ctl", [true]);
-      }
-    });
-  }
-
-  /**
-   * 睡眠函数，睡眠time时间后执行代码
-   * @param time 单位ms
-   * @returns {Promise<unknown>}延迟函数
-   */
-  static sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
-
-  /**
-   * 断开连接,重置所有的aoa_locator
-   * @param callback 断开连接成功的回调
-   */
-  static disconnect(callback) {
-    mqttx.station_list.forEach((station) => (station.status = false));
-    if (mqttx.client === null || !mqttx.client.connected) return;
-    this.client.end(true, {}, callback);
-    ipcRenderer.send("end_server");
-  }
-
-  // 订阅
-  static subscribe(topics, callback) {
-    if (this.client && this.client.connected) this.client.subscribe(topics, { qos: 0 }, callback);
-  }
-
-  // 取消订阅
-  static unsubscribe(topics) {
-    if (!this.client || !this.client.connected) return;
-    this.client.unsubscribe(topics, (err, packet) => {
-      if (err) console.log("unsubscribe error: " + err);
-      else console.info("unsubscribe success: " + packet);
-    });
-  }
-
-  // 重新设置clientID
-  static setId(id = "default-id") {
-    this.options.clientId = id;
-    if (this.client && this.client.connected) this.connect();
-  }
-
-  // 注册消息函数回调
-  static set_message_callback(m) {
-    this.messages = m;
-  }
-
-  // 消息到达时执行函数
-  static message(topic, message) {
-    // 获取topic中包含的stationId 和 tagId
-    const s = topic.split("/");
-    const stationsId = s[s.length - 2];
-    const tagId = s[s.length - 1];
-    // 初始化
-    if (!mqttx.stations.has(stationsId)) mqttx.stations.set(stationsId, []);
-    if (!mqttx.tags.has(tagId)) mqttx.tags.set(tagId, []);
-    // 存储消息
-    const i = JSON.parse(message.toString());
-    i["cId"] = mqttx.options.clientId;
-    // console.log(mqttx.station_map);
-    i["x"] = mqttx.station_map[stationsId]["position"].x;
-    i["y"] = mqttx.station_map[stationsId]["position"].y;
-    i["z"] = mqttx.station_map[stationsId]["position"].z;
-    mqttx.stations.get(stationsId).push(i);
-    mqttx.tags.get(tagId).push(i);
-    // 只能够存储激活的tag数据
-    if (tagId === mqttx.active_tag) mqttx.msgs.push(i);
-    // 执行注册的函数
-    mqttx.messages(topic, `${stationsId}:${tagId} -> ${message.toString()}`);
-    // 存储数据,按照id作为文件夹进行划分
-    if (mqttx.stations.get(stationsId).length >= MAX_LEN) {
-      mqttx.save(mqttx.stations.get(stationsId), `${stationsId}/${new Date().toLocaleDateString()}.json`);
-      mqttx.stations.set(stationsId, []);
+    /**
+     * mqtt 连接服务器函数
+     * @param url{string} 连接地质
+     * @param s{function} 连接成功回调
+     * @param f{function} 连接失败回调
+     */
+    static connect(url = "ws://localhost:9001", s = function () {
+    }, f = function () {
+    }) {
+        mqttx.url = url;
+        mqttx.client = mqtt.connect(mqttx.url, mqttx.options);
+        mqttx.client.on("connect", () => {
+            s();
+            this.client.on("message", mqttx.message);
+        });
+        mqttx.client.on("error", (err) => {
+            console.log("client error", err);
+        });
+        mqttx.sleep(300).then(() => {
+            if (mqttx.client.connected === false) {
+                f();
+                // 重新启动 mosquitto
+                ipcRenderer.send("mosquitto_ctl", [true]);
+            }
+        });
     }
-    if (mqttx.tags.get(tagId).length >= MAX_LEN) {
-      mqttx.save(mqttx.tags.get(tagId), `${tagId}/${new Date().toLocaleDateString()}.json`);
-      mqttx.tags.set(tagId, []);
+
+    /**
+     * 睡眠函数，睡眠time时间后执行代码
+     * @param time 单位ms
+     * @returns {Promise<unknown>}延迟函数
+     */
+    static sleep(time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
     }
-    // 判断是否需要发送数据
-    if (mqttx.msgs.length >= 6) {
-      upload_aoa_raw_data(mqttx.msgs);
-      console.log(mqttx.msgs);
-      mqttx.msgs = [];
+
+    /**
+     * 断开连接,重置所有的aoa_locator
+     * @param callback 断开连接成功的回调
+     */
+    static disconnect(callback) {
+        mqttx.station_list.forEach((station) => (station.status = false));
+        if (mqttx.client === null || !mqttx.client.connected) return;
+        this.client.end(true, {}, callback);
+        ipcRenderer.send("end_server");
     }
-  }
 
-  // 存储数据
-  static save(data, name) {
-    ipcRenderer.send("write", ["data", name, data]);
-  }
+    // 订阅
+    static subscribe(topics, callback) {
+        if (this.client && this.client.connected) this.client.subscribe(topics, {qos: 0}, callback);
+    }
 
-  // 默认主题
-  static defaultTopic() {
-    return "silabs/aoa/angle";
-  }
+    // 取消订阅
+    static unsubscribe(topics) {
+        if (!this.client || !this.client.connected) return;
+        this.client.unsubscribe(topics, (err, packet) => {
+            if (err) console.log("unsubscribe error: " + err);
+            else console.info("unsubscribe success: " + packet);
+        });
+    }
 
-  // 默认订阅
-  static defaultSubscribe(callback) {
-    this.subscribe(`${this.defaultTopic()}/#`, callback);
-  }
+    // 重新设置clientID
+    static setId(id = "default-id") {
+        this.options.clientId = id;
+        if (this.client && this.client.connected) this.connect();
+    }
 
-  /**
-   * 改变基站状态，启动基站服务
-   * @param index{number} 基站序列号
-   * @param status{boolean} 状态
-   */
-  static station_status_ctl(index, status) {
-    if (status && !mqttx.station_list[index].status) mqttx.active_station_num += 1;
-    else mqttx.active_station_num -= 1;
-    mqttx.active_station_num = mqttx.active_station_num < 0 ? 0 : mqttx.active_station_num;
-    mqttx.station_list[index].status = status;
-    ipcRenderer.send("locator_ctl", [mqttx.station_list[index].net, status]);
-  }
+    // 注册消息函数回调
+    static set_message_callback(m) {
+        this.messages = m;
+    }
+
+    // 消息到达时执行函数
+    static message(topic, message) {
+        // 获取topic中包含的stationId 和 tagId
+        const s = topic.split("/");
+        const stationsId = s[s.length - 2];
+        const tagId = s[s.length - 1];
+        // 初始化
+        if (!mqttx.stations.has(stationsId)) mqttx.stations.set(stationsId, []);
+        if (!mqttx.tags.has(tagId)) mqttx.tags.set(tagId, []);
+        // 存储消息
+        const i = JSON.parse(message.toString());
+        i["cId"] = mqttx.options.clientId;
+        // console.log(mqttx.station_map);
+        i["x"] = mqttx.station_map[stationsId]["position"].x;
+        i["y"] = mqttx.station_map[stationsId]["position"].y;
+        i["z"] = mqttx.station_map[stationsId]["position"].z;
+        mqttx.stations.get(stationsId).push(i);
+        mqttx.tags.get(tagId).push(i);
+        // 只能够存储激活的tag数据
+        if (tagId === mqttx.active_tag) mqttx.msgs.push(i);
+        // 执行注册的函数
+        mqttx.messages(topic, `${stationsId}:${tagId} -> ${message.toString()}`);
+        // 存储数据,按照id作为文件夹进行划分
+        if (mqttx.stations.get(stationsId).length >= MAX_LEN) {
+            mqttx.save(mqttx.stations.get(stationsId), `${stationsId}/${new Date().toLocaleDateString()}.json`);
+            mqttx.stations.set(stationsId, []);
+        }
+        if (mqttx.tags.get(tagId).length >= MAX_LEN) {
+            mqttx.save(mqttx.tags.get(tagId), `${tagId}/${new Date().toLocaleDateString()}.json`);
+            mqttx.tags.set(tagId, []);
+        }
+        // 判断是否需要发送数据
+        if (mqttx.msgs.length >= 6) {
+            upload_aoa_raw_data(mqttx.msgs);
+            console.log(mqttx.msgs);
+            mqttx.msgs = [];
+        }
+    }
+
+    // 存储数据
+    static save(data, name) {
+        ipcRenderer.send("write", ["data", name, data]);
+    }
+
+    // 默认主题
+    static defaultTopic() {
+        return "silabs/aoa/angle";
+    }
+
+    // 默认订阅
+    static defaultSubscribe(callback) {
+        this.subscribe(`${this.defaultTopic()}/#`, callback);
+    }
+
+    /**
+     * 改变基站状态，启动基站服务
+     * @param index{number} 基站序列号
+     * @param status{boolean} 状态
+     */
+    static station_status_ctl(index, status) {
+        if (status && !mqttx.station_list[index].status) mqttx.active_station_num += 1;
+        else mqttx.active_station_num -= 1;
+        mqttx.active_station_num = mqttx.active_station_num < 0 ? 0 : mqttx.active_station_num;
+        mqttx.station_list[index].status = status;
+        ipcRenderer.send("locator_ctl", [mqttx.station_list[index].net, status]);
+    }
+
+    /**
+     * 通过id 改变基站状态
+     * @param id 基站id
+     * @param status 状态
+     */
+    static station_status_ctl_by_id(id, status) {
+        let index = this.station_list.findIndex((item) => item.id === id)
+        this.station_status_ctl(index, status)
+    }
 }
--- src/views/error_fix_view.vue
+++ src/views/error_fix_view.vue
@@ -1,141 +1,253 @@
 <template>
-  <el-container>
-    <el-header>
-      <el-row>
-        <el-col :span="10" style="text-align: left">
-          <h1>error fix</h1>
-        </el-col>
-        <el-divider></el-divider>
-      </el-row>
-    </el-header>
-    <el-main>
-      <el-steps :active="active" finish-status="success" align-center>
-        <el-step title="station" />
-        <el-step title="tags" />
-        <el-step title="start" />
-      </el-steps>
-      <el-container style="margin-top: 20px">
-        <div v-if="active == 0" class="main">
-          <el-table :data="this.$mqttx.station_list" highlight-current-row @current-change="stations_change">
-            <el-table-column prop="id" label="id" width="195" />
-            <el-table-column prop="name" label="name" width="195" />
-            <el-table-column prop="net" label="net" width="195" />
-            <el-table-column prop="status" label="status" width="195" />
-          </el-table>
-        </div>
-        <div v-if="active == 1" class="main">
-          <el-table :data="this.$mqttx.tag_list" highlight-current-row @selection-change="tags_change">
-            <el-table-column type="selection" width="50" />
-            <el-table-column prop="id" label="id" width="230" />
-            <el-table-column prop="name" label="name" width="230" />
-            <el-table-column prop="status" label="status" width="230" />
-          </el-table>
-        </div>
-        <div v-if="active == 2" class="main">
-          <el-row :gutter="20">
-            <el-col :span="14" class="a">
-              <el-row>
-                <el-col><p>station</p></el-col>
-              </el-row>
-              <el-row>
-                <el-col class="b">
-                  <el-card>
-                    {{ this.slected_station.id }}
-                  </el-card>
+    <el-container>
+        <el-header>
+            <el-row>
+                <el-col :span="10" style="text-align: left">
+                    <h1>error fix</h1>
                 </el-col>
-              </el-row>
-              <el-divider />
-              <el-row>
-                <el-col><p>tags</p></el-col>
-              </el-row>
-              <el-row>
-                <el-col v-for="(o, i) in this.slected_tags.length" :key="o" class="c">
-                  <el-card>
-                    {{ this.slected_tags[i].id }}
-                  </el-card>
+                <el-divider></el-divider>
+            </el-row>
+        </el-header>
+        <el-main>
+            <el-steps :active="active" finish-status="success" align-center>
+                <el-step title="station"/>
+                <el-step title="tags"/>
+                <el-step title="start"/>
+            </el-steps>
+            <el-container style="margin-top: 20px">
+                <div v-if="active === 0" class="main">
+                    <el-table :data="this.$mqttx.station_list" highlight-current-row @current-change="stations_change">
+                        <el-table-column prop="id" label="id" width="195"/>
+                        <el-table-column prop="name" label="name" width="195"/>
+                        <el-table-column prop="net" label="net" width="195"/>
+                        <el-table-column prop="status" label="status" width="195"/>
+                    </el-table>
+                </div>
+                <div v-if="active === 1" class="main">
+                    <el-table :data="this.$mqttx.tag_list" highlight-current-row @selection-change="tags_change">
+                        <el-table-column type="selection" width="50"/>
+                        <el-table-column prop="id" label="id" width="230"/>
+                        <el-table-column prop="name" label="name" width="230"/>
+                        <el-table-column prop="status" label="status" width="230"/>
+                    </el-table>
+                </div>
+                <div v-if="active === 2" class="main">
+                    <el-row :gutter="20">
+                        <el-col :span="14" class="a">
+                            <el-row>
+                                <el-col><p>station</p></el-col>
+                            </el-row>
+                            <el-row>
+                                <el-col class="b">
+                                    <el-card>
+                                        {{ this.selected_station.id }}
+                                    </el-card>
+                                </el-col>
+                            </el-row>
+                            <el-divider/>
+                            <el-row>
+                                <el-col><p>tags</p></el-col>
+                            </el-row>
+                            <el-row>
+                                <el-col v-for="(o, i) in this.selected_tags.length" :key="o" class="c">
+                                    <el-card @click="click_tag(i)">
+                                        {{ this.selected_tags[i].id }}
+                                        x: {{
+                                            this.selected_tags[i].position ? this.selected_tags[i].position.x : "null"
+                                        }},
+                                        y: {{
+                                            this.selected_tags[i].position ? this.selected_tags[i].position.y : "null"
+                                        }},
+                                        z: {{
+                                            this.selected_tags[i].position ? this.selected_tags[i].position.z : "null"
+                                        }}
+                                    </el-card>
+                                </el-col>
+                            </el-row>
+                        </el-col>
+                        <el-col :span="10" class="a">
+                            <el-row>
+                                <el-col :span="24" class="d">
+                                    <el-button type="primary" style="width: 100px; height: 100px; border-radius: 50px"
+                                               @click="start">
+                                        start
+                                    </el-button>
+                                </el-col>
+                            </el-row>
+                            <el-row>
+                                <el-col :span="24" class="d">
+                                    <p style="text-align: center">current record times: {{ this.record_times }}</p>
+                                </el-col>
+                            </el-row>
+                        </el-col>
+                    </el-row>
+                </div>
+                <div v-if="active === 3" class="main">3</div>
+            </el-container>
+        </el-main>
+        <el-footer>
+            <el-row>
+                <el-col :span="12">
+                    <el-button @click="minus">
+                        <el-icon>
+                            <ArrowLeft/>
+                        </el-icon>
+                    </el-button>
                 </el-col>
-              </el-row>
+                <el-col :span="12">
+                    <el-button @click="plus">
+                        <el-icon>
+                            <ArrowRight/>
+                        </el-icon>
+                    </el-button>
+                </el-col>
+            </el-row>
+        </el-footer>
+    </el-container>
+    <el-dialog v-model="add_tags_position" title="add position" width="400px">
+        <el-row>
+            <el-col :span="6"><p>tag_x</p></el-col>
+            <el-col :span="18">
+                <el-input v-model="selected_tag.position.x"></el-input>
             </el-col>
-            <el-col :span="10" class="a">
-              <el-row>
-                <el-col :span="24" class="d"><el-button type="primary" style="width: 100px; height: 100px; border-radius: 50px">start</el-button></el-col>
-              </el-row>
-              <el-row>
-                <el-col :span="24" class="d">
-                  <p style="text-align: center">current record times: {{ this.record_times }}</p>
-                </el-col>
-              </el-row>
+        </el-row>
+        <el-row>
+            <el-col :span="6"><p>tag_y</p></el-col>
+            <el-col :span="18">
+                <el-input v-model="selected_tag.position.y"></el-input>
             </el-col>
-          </el-row>
-        </div>
-        <div v-if="active == 3" class="main">3</div>
-      </el-container>
-    </el-main>
-    <el-footer>
-      <el-row>
-        <el-col :span="12">
-          <el-button @click="minus">
-            <el-icon><ArrowLeft /></el-icon>
-          </el-button>
-        </el-col>
-        <el-col :span="12">
-          <el-button @click="plus">
-            <el-icon><ArrowRight /></el-icon>
-          </el-button>
-        </el-col>
-      </el-row>
-    </el-footer>
-  </el-container>
-  <el-diaolog> </el-diaolog>
+        </el-row>
+        <el-row>
+            <el-col :span="6"><p>tag_z</p></el-col>
+            <el-col :span="18">
+                <el-input v-model="selected_tag.position.z"></el-input>
+            </el-col>
+        </el-row>
+    </el-dialog>
 </template>
 
 <script>
+import {store} from "@/utils/store";
+import {ElMessage} from "element-plus";
+
+const ipcRenderer = window.require("electron").ipcRenderer;
 export default {
-  data: function () {
-    return {
-      active: 0,
-      slected_station: {},
-      slected_tags: [{}, {}],
-      record_times: 0,
-    };
-  },
-  created: function () {
-    console.log(this.$mqttx.tag_list);
-  },
-  methods: {
-    minus() {
-      this.active -= 1;
-      this.active = this.active >= 0 ? this.active : 0;
+    data: function () {
+        return {
+            active: 0,
+            selected_station: {},
+            selected_tags: [{}, {}],
+            selected_tag: {},
+            record_times: 0,
+            add_tags_position: false,
+            status: "start",
+            files: [],
+            upload_files: [],
+        };
     },
-    plus() {
-      this.active += 1;
-      this.active = this.active <= 3 ? this.active : 3;
+    created: function () {
     },
-    stations_change(val) {
-      this.slected_station = val;
+    computed: {
+        cur_url: {
+            get: function () {
+                return store.cur_url;
+            },
+            set: function (val) {
+                store.set_cur_url(val);
+            },
+        },
     },
-    tags_change(val) {
-      this.slected_tags = val;
+    methods: {
+        minus() {
+            this.active -= 1;
+            this.active = this.active >= 0 ? this.active : 0;
+        },
+        plus() {
+            this.active += 1;
+            this.active = this.active <= 3 ? this.active : 3;
+        },
+        /**
+         * 基站选择变更，选择之后会读取基站相关的误差文件，进行files的初始化
+         * @param val 选择后的基站
+         */
+        stations_change(val) {
+            this.selected_station = val
+            this.update_files(this.selected_station.id)
+        },
+        /**
+         * 通过id 读取station的误差采集数据
+         * @param id
+         */
+        update_files(id) {
+            ipcRenderer.send("read", ["error_files", id])
+            ipcRenderer.once("data", (event, arg) => {
+                this.files = arg;
+            })
+        },
+        tags_change(val) {
+            this.selected_tags = val;
+        },
+        click_tag(i) {
+            this.selected_tag = this.selected_tags[i]
+            if (this.selected_tag.position === undefined) this.selected_tag.position = {x: 0, y: 0, z: 0}
+            this.add_tags_position = true
+        },
+        /**
+         * 开始采集数据 \n
+         * 重新连接mqtt服务、重新订阅 \n
+         * 数据都保存至 this.$mqttx.tags 中，多次采集
+         */
+        start() {
+            if (this.$mqttx.client === undefined || this.$mqttx.client.connected === false)
+                this.$mqttx.connect(this.cur_url.value, this.s, this.f)
+            this.status = "collecting"
+            this.$mqttx.station_status_ctl_by_id(this.selected_station.id, true)
+            setTimeout(() => {
+                this.$mqttx.station_status_ctl_by_id(this.selected_station.id, false)
+                this.status = "start"
+                this.record_times += 1
+                this.upload_files.push(this.$mqttx.msgs)
+            }, 10000)
+
+        },
+        s() {
+            // 自动订阅主题
+            this.$mqttx.defaultSubscribe(() => {
+                ElMessage({type: "success", message: "subscribe success"})
+                this.code += "connect & subscribe success" + "\n";
+            });
+            store.set_main_connect_status(true);
+        },
+        f() {
+            ElMessage({type: "error", message: "connect failed please retry"});
+            this.code += "connect failed \n";
+            ipcRenderer.send("mosquitto_ctl", [true])
+            store.set_main_connect_status(false);
+        },
     },
-  },
 };
 </script>
 
 <style scoped>
 .a {
-  height: 400px;
+    height: 400px;
 }
+
 .main {
-  width: 785px;
+    width: 785px;
 }
+
 .b {
-  height: 70px;
+    height: 70px;
 }
+
 .c {
-  height: 70px;
+    height: 70px;
 }
+
 .d {
-  height: 120px;
-  margin-top: 100px;
+    height: 120px;
+    margin-top: 100px;
 }
+
 </style>
